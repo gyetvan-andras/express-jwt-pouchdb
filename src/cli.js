efe968987d9032cbcc1233643cd8947d74b49a69
Gyetván András <gyetvan.andras@me.com>
2017-01-07 17:11:13 +0100

    Revert "- reject unauthorized requests"
    This reverts commit 3490fe1bd9c4588630cce680d7082a3464bebffe.

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
-import minimist from "minimist"
-import PouchDB from "pouchdb"
-import path from "path"
-import mkdirp from "mkdirp"
+import minimist from "minimist";
+import PouchDB from "pouchdb";
+import path from "path";
+import mkdirp from "mkdirp";
 
 // using standard require so rollup doesn't include it
-const createApp = require("./")
+const createApp = require("./");
 
 let argv = minimist(process.argv.slice(2), {
 	string: [ ],
@@ -20,44 +20,44 @@ let argv = minimist(process.argv.slice(2), {
 		dir: "./",
 		port: process.env.PORT || 3000
 	}
-})
+});
 
 if (argv.help) {
-	console.log("halp plz")
-	process.exit(0)
+	console.log("halp plz");
+	process.exit(0);
 }
 
 if (argv.version) {
-	let pkg = require("./package.json")
-	console.log("%s %s", pkg.name, pkg.version || "edge")
-	process.exit(0)
+	let pkg = require("./package.json");
+	console.log("%s %s", pkg.name, pkg.version || "edge");
+	process.exit(0);
 }
 
 function panic(e) {
-	console.error(e.stack || e)
-	process.exit(1)
+	console.error(e.stack || e);
+	process.exit(1);
 }
 
-var opts = {}
+var opts = {};
 
-opts.prefix = path.resolve(argv["dir"]) + path.sep
-mkdirp.sync(opts.prefix)
+opts.prefix = path.resolve(argv["dir"]) + path.sep;
+mkdirp.sync(opts.prefix);
 
 if (argv["level-prefix"]) {
-	opts.prefix = argv["level-prefix"]
+	opts.prefix = argv["level-prefix"];
 }
 if (argv["in-memory"]) {
-	opts.db = require("memdown")
+	opts.db = require("memdown");
 } else if (argv["level-backend"]) {
-	opts.db = require(argv["level-backend"])
+	opts.db = require(argv["level-backend"]);
 } else if (argv["sqlite"]) {
-	opts.adapter = "websql"
+	opts.adapter = "websql";
 }
 
-const app = createApp(PouchDB.defaults(opts), argv)
+const app = createApp(PouchDB.defaults(opts), argv);
 
 const server = app.listen(argv.port, () => {
-	console.log(`HTTP server listening on port ${server.address().port}.`)
-})
+	console.log(`HTTP server listening on port ${server.address().port}.`);
+});
 
-server.on("error", panic)
+server.on("error", panic);
